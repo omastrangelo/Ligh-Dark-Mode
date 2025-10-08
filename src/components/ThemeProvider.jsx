d9d9f64157cc8990c49968ae64e4f2c7cb59c1e1
omastrangelo <octamastrangelo@gmail.com>
2025-10-08 10:25:50 -0300

    Correciones en estilos

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -1,25 +1,21 @@
-import { useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 
-export const ThemeProvider = ()=>{
+export const ThemeProvider = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleTheme = () => setDarkMode(!darkMode);
 
-    const [darkMode, setDarkMode] = useState(false)
-    const toggleTheme = () => setDarkMode(!darkMode);
+  useEffect(() => {
+    document.body.className = darkMode ? "dark" : "light";
+  }, [darkMode]);
 
-   useEffect(() => {
-      document.body.className = darkMode ? "dark" : "light";
-    }, [darkMode]);
-
-    return(
-        <div className={darkMode ? "app dark" : "app light"}>
+  return (
+      <div className={darkMode ? "app dark" : "app light"}>
         <h1>{darkMode ? "🌙 Modo Oscuro" : "☀️ Modo Claro"}</h1>
         <button onClick={toggleTheme}>
           Cambiar a {darkMode ? "modo claro" : "modo oscuro"}
         </button>
       </div>
-    );
+  );
+};
 
-}
-
-
-export default ThemeProvider
-
+export default ThemeProvider;
